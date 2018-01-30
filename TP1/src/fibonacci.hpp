a8956bbb4a6177e1c0d20fddae8e69aca5b1d0ee
Maxime <leclet.maxime@busi-part.com>
2018-01-30 12:59:58 +0100

    Arrivee test unitaires

---
+++ TP1/src/fibonacci.hpp
@@ -0,0 +1,7 @@
+#ifndef FIBONACCI_HPP
+#define FIBONACCI_HPP
+
+int fibonacciRecursif(int nb);
+int fibonacciIteratif(int nb);
+
+#endif
--- TP1/src/main_test.cpp
+++ TP1/src/main_test.cpp
@@ -3,7 +3,7 @@
 
 int main() {
 
-  std::cout << fibonacciRecursif(7)  << std::endl;
+  
 
   return 0;
 }
