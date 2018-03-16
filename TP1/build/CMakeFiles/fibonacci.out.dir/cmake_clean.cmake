2b0621a42847a5340a641a21c8021b99a70f7367
Maxime <leclet.maxime@busi-part.com>
2018-03-16 14:46:08 +0100

    Push TP1

---
+++ TP1/build/CMakeFiles/fibonacci.out.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fibonacci.out.dir/src/main.cpp.o"
+  "CMakeFiles/fibonacci.out.dir/src/fibonacci.cpp.o"
+  "CMakeFiles/fibonacci.out.dir/src/fibonacci_test.cpp.o"
+  "fibonacci.out.pdb"
+  "fibonacci.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fibonacci.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
